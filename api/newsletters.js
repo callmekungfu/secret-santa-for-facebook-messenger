450e64549457389481fc15b51121866b7a614d47
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2018-11-11 02:08:48 -0500

    Moved Database Config

--- api/newsletters.js
+++ api/newsletters.js
@@ -1,21 +1,7 @@
 const mongoose = require('mongoose');
-const dbconfig = require('../database-config');
-
-const mongoDB = dbconfig.address;
 const Schema  = mongoose.Schema;
 
-mongoose.connect(mongoDB, {
-    useNewUrlParser: true,
-    dbName: 'kithconDB'
-});
-
-mongoose.Promise = global.Promise;
-
-const database = mongoose.connection;
-
-database.on('error', console.error.bind(console, 'MongoDB connection error: '));
-
-let subscriber = new Schema({
+const subscriber = new Schema({
     email: String,
     first_name: String,
     last_name: String,
