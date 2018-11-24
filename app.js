1e7aa4840d35de0312a58756d816d3d6b89a3656
callmekungfu <callmekungfu@users.noreply.github.com>
2018-11-24 17:06:47 -0500

    Major Feature Update - Sharing Invitation - Getting started btn - persistent menu

--- app.js
+++ app.js
@@ -26,26 +26,19 @@ module.exports = app;
 
 // Accepts POST requests at the /webhook endpoint
 app.post('/webhook', (req, res) => {
-  // Parse the request body from the POST
   let body = req.body;
-  console.log(body);
-  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
     body.entry.forEach(({
       messaging
     }) => {
 
-      // Gets the body of the webhook event
       let webhook_event = messaging[0];
       console.log(webhook_event);
 
-      // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log(`Sender PSID: ${sender_psid}`);
 
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
@@ -54,37 +47,29 @@ app.post('/webhook', (req, res) => {
 
     });
 
-    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
 
   } else {
-    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
 });
 
-// Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
   const VERIFY_TOKEN = process.env.TOKEN;
 
-  // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
-  // Check if a token and mode were sent
   if (mode && token) {
 
-    // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-      // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
 
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
   }
@@ -106,11 +91,23 @@ app.get('/options', (req, res) => {
 
 app.get('/optionspostback', (req, res) => {
   let body = req.query;
-  let response = {
-    "text": `Yay! Your ${body.name} party is on its way! Here are the details: \nLocation: ${body.location}\nDate: ${moment(body.date).format('MMMM Do YYYY, h:mm a')}\nBudget: $${body.budget}\nThere are currently no friends in the party.`
-  };
+  let response = afterPartyCreation(body,body.psid);
   res.status(200).send('Please close this window to return to the conversation thread.');
   callSendAPI(body.psid, response);
+});
+
+app.get('/invitation', (req, res) => {
+  let referer = req.get('Referer');
+  if (referer) {
+    if (referer.indexOf('www.messenger.com') >= 0) {
+      res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
+    } else if (referer.indexOf('www.facebook.com') >= 0) {
+      res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
+    }
+    res.sendFile('public/invitation.html', {
+      root: __dirname
+    });
+  }
 });
 
 // Handles messages sent to the bot
@@ -120,7 +117,6 @@ function handleMessage(sender_psid, received_message) {
     switch (received_message.text.replace(/[^\w\s]/gi, '').trim().toLowerCase()) {
       case "create party":
         response = setRoomPreferences(sender_psid);
-        console.log(response)
         break;
       default:
         response = {
@@ -133,8 +129,24 @@ function handleMessage(sender_psid, received_message) {
       "text": `Sorry I don't understand you...`
     }
   }
-
   callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, postback) {
+  let response;
+  switch(postback.payload) {
+    case 'get_started':
+      callSendAPI(sender_psid, {
+        "text": `Let's get started!🎉🎉`
+      });
+      callSendAPI(sender_psid, {
+        "text": `Secret santa for friends will help you manage and run a secret santa party with your friends, all you have to do is provide the details and share the party with your friends!`
+      });
+      break;
+    case 'NEW_PARTY':
+      callSendAPI(sender_psid, setRoomPreferences(sender_psid));
+      break;
+  }
 }
 
 // Define the template and webview
@@ -144,7 +156,7 @@ function setRoomPreferences(sender_psid) {
       type: "template",
       payload: {
         template_type: "button",
-        text: "Sounds good, let's get your party going! 🎉🎉",
+        "text": "Sounds good, let's get your party going! 🎉🎉",
         buttons: [{
           type: "web_url",
           url: SERVER_URL + "/options",
@@ -155,9 +167,53 @@ function setRoomPreferences(sender_psid) {
         {
           type: "web_url",
           url: SERVER_URL + "/help",
-          title: "What are paries?",
+          title: "What are parties?",
           webview_height_ratio: 'full',
           messenger_extensions: true
+        }]
+      }
+    }
+  };
+  return response;
+}
+
+function afterPartyCreation(body, sender_psid) {
+  let response = {
+    attachment: {
+      type: "template",
+      payload: {
+        template_type: "button",
+        text: `Yay! Your ${body.name} party is on its way! Here are the details: \n\nLocation: ${body.location}\nDate: ${moment(body.date).format('MMMM Do YYYY, h:mm a')}\nBudget: $${body.budget}\n\nInvite your friends now!`,
+        buttons: [{
+          type: "element_share",
+          share_contents: {
+            attachment: {
+              type: "template",
+              payload: {
+                template_type: 'generic',
+                elements: [{
+                  title: `You are invited to ${body.name} Party!`,
+                  subtitle: `More Details:\n\nLocation: ${body.location}\nDate:${moment(body.date).format('MMMM Do YYYY, h:mm a')}\nBudget:$${body.budget}\n\nJoin Now!`,
+                  image_url: SERVER_URL + '/images/santa.jpg',
+                  default_action: {
+                    type: 'web_url',
+                    url: SERVER_URL + '/invitation?party_id=hellothere',
+                    messenger_extensions: true,
+                    webview_height_ratio: 'tall'
+                  },
+                  buttons: [
+                    {
+                      type: "web_url",
+                      title: "Join Now!",
+                      url: SERVER_URL + '/invitation',
+                      messenger_extensions: true,
+                      webview_height_ratio: 'tall'
+                    }
+                  ]
+                }]
+              }
+            }
+          }
         }]
       }
     }
