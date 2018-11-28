e65258aea0b6048f6101d79d5a16ec89428cc75b
callmekungfu <callmekungfu@users.noreply.github.com>
2018-11-27 22:13:37 -0500

    testing

--- app.js
+++ app.js
@@ -702,7 +702,7 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!')
+      console.log('message sent!', res, body)
     } else {
       console.error("Unable to send message:" + err);
     }
